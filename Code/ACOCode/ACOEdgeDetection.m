b7745befa0487417e8b2c35fa5880e0f09b251ef
Korstanje <l.a.korstanje@ziggo.nl>
2016-05-15 11:11:55 +0200

    Gridsearch is nu toegevoegd. Er moet nog wel een evaluatie in gezet worden

--- Code/ACOCode/ACOEdgeDetection.m
+++ Code/ACOCode/ACOEdgeDetection.m
@@ -1,9 +1,10 @@
 function [ pheromone_map ] = ACOEdgeDetection( input_file, heuristic_name, ant_creation_name, ant_initialization_name, ant_possible_step_name, num_iterations, ant_num_steps, ant_memory_length, alpha, beta, rho, phi )
 %ACOEDGEDETECTION creates a pheromone map of the edges for a given image.
-% INPUT_FILE the location of the image
+% INPUT_FILE the location of the image to be processed.
 %
 % HEURISTIC_NAME the name of the function that calculates the heuristic. 
-%   It should take the input image as argument and return a heuristic map
+%   It should take the input image as argument and return a
+%   heuristic map that is the size of the image.
 %
 % ANT_CREATION_NAME the name of the function that calculates the amount of
 % ants.
@@ -21,22 +22,22 @@ function [ pheromone_map ] = ACOEdgeDetection( input_file, heuristic_name, ant_c
 %   It should take as argument the input image and the ants location and it
 %   should return a list(num_pos_steps, 2) of possible steps.
 %
-% NUM_ITERATIONS the number of iterations
+% NUM_ITERATIONS the number of iterations.
 %
-% ANT_NUM_STEPS the number of steps each ant takes per iteration
+% ANT_NUM_STEPS the number of steps each ant takes per iteration.
 %
-% ANT_MEMORY_LENGTH the number of steps the ant keeps in his memory
+% ANT_MEMORY_LENGTH the number of steps the ant keeps in his memory.
 %
 % ALPHA the influence of the pheromone levels on deciding which step to
-% take
+% take.
 %
 % BETA the influence of the heuristic information on deiciding which step
-% to take
+% to take.
 %
-% RHO evaporation rate: pheromone decay after individual ant step
+% RHO evaporation rate: pheromone decay after individual ant step.
 %
 % PHI pheromone decay coefficient: pheromone decay after all ants have done
-% a step
+% a step.
 
 
 % Load image
